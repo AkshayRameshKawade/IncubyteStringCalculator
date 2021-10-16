8e0d3f2ec1c4c8532f232cc14a3c985b3f8b26b4
Akshay Kawade <kawadeakshay011@gmail.com>
2021-10-16 21:19:03 +0530

    Support different delimiters

--- StringCalculator/src/test/java/calculator/StringCalculatorShould.java
+++ StringCalculator/src/test/java/calculator/StringCalculatorShould.java
@@ -24,5 +24,19 @@ class StringCalculatorShould {
 		// return sum of comma seperated numbers
 		assertEquals(3, stringCalculator.add("1,2"));
 	}
+    @Test
+	void stringWithUnknownNumberWithCommaDelimterReturnNumberAsSum() {
+		StringCalculator stringCalculator = new StringCalculator();
+		// Allow the Add method to handle an unknown amount of numbers
+		assertEquals(10, stringCalculator.add("1,2,3,4"));
+	}
+
+	@Test
+	void stringWithNewLinesAndCommBetweenNumbersAndReturnSum() {
+		StringCalculator stringCalculator = new StringCalculator();
+		// Allow the Add method to handle new lines between numbers (instead of commas)
+		assertEquals(6, stringCalculator.add("1\n2,3"));
+	}
+
 
 }
